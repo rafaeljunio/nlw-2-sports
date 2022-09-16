db4ff8730c39034b5b8522e46bb1cf8a38eb9085
Rafael Souza <rafael.souza@animasales.com.br>
2022-09-15 21:39:12 -0300

    class 03: setup and modeling tables prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'A' },
     { id: 1, name: 'A' },
